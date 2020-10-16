70123964c5f057b372422538c35d3d52fceacda1
nikhil1526 <66792565+nikhil1526@users.noreply.github.com>
2020-10-16 23:20:28 +0530

    Create Q2.java

---
+++ 18103070_Assignment4/Q2.java
@@ -0,0 +1,19 @@
+
+
+class Q2
+{
+	public static void main(String[] args)
+	{
+		int number=-1;
+		byte b=(byte)number;
+	
+		char c1=(char)b;
+	
+		int final_value=c1;
+		System.out.println("Initial number: "+number);
+		System.out.println("In Byte: "+b);
+		System.out.println("In Char "+c1);
+		System.out.println("Final: "+final_value);
+
+	}
+}
